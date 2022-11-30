674349fc8e98054e47d0129d1a77f98e29081175
toleman84 <gdponcemarsiglia@gmail.com>
2022-11-30 09:57:17 -0800

    prototype

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int main(int ac, char **argv)
 		/* check if the getline function failed or reached EOF or user use CTRL+D */
 		if (nchars_read == -1)
 		{
-			printf("Existing shel....\n");
+			printf("Exiting shell....\n");
 			return (-1);
 		}
 
