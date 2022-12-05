b39fc73f71b53f4a77a1a0266a08a2485cdaaee6
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2022-12-05 01:39:07 -0800

    add

--- main.h
+++ main.h
@@ -10,7 +10,8 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
+#include <ctype.h>
+#include <sys/wait.h>
 
 /* prototypes */
 
@@ -26,5 +27,9 @@ char *_strcpy(char *dest, char *src);
 char *_strdup(char *str);
 char *_strcat(char *dest, const char *src);
 int _strlen(const char *s);
+
+/* exit func */
+int exit_func(void);
+
 
 #endif
